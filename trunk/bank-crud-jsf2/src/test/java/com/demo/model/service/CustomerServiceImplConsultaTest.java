44318e390ce23e39ccdb6320c744225357e0d0b9
alexrojas235@gmail.com <alexrojas235@gmail.com@97d5706b-13d2-8499-cc0b-5c95a49f2aa6>
2013-05-06 09:02:21 +0000

    Creados test para los metodos de consulta Autor: Edward Alexander Rojas Clavijo Solicitante: Edward Alexander Rojas Clavijo
    Creada clase para testear los metodos de consulta
    Clase: CustomerServiceImplConsultaTest
    
    Creados test para los metodos de cosulta: count, findById y findAll
    Clase: CustomerServiceImplConsultaTest

---
+++ trunk/bank-crud-jsf2/src/test/java/com/demo/model/service/CustomerServiceImplConsultaTest.java
@@ -0,0 +1,110 @@
+package com.demo.model.service;
+
+import com.demo.model.dao.CustomerDAO;
+import com.demo.pojo.Card;
+import com.demo.pojo.Customer;
+import com.demo.pojo.Gender;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Alexander
+ */
+public class CustomerServiceImplConsultaTest extends TestCase {
+
+    CustomerServiceImpl customerServiceImpl;
+    List listaClientes;
+
+    public CustomerServiceImplConsultaTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        CustomerDAO dao = new CustomerDAO();
+        dao.postConstruct();
+        customerServiceImpl = new CustomerServiceImpl();
+        customerServiceImpl.postConstruct();
+        customerServiceImpl.setDao(dao);
+
+        customerServiceImpl.deleteAll();
+
+        Customer customer = new Customer();
+        customer.setName("TestCustomer");
+        Calendar fecha1 = Calendar.getInstance();
+        fecha1.setTimeInMillis(0);
+        fecha1.set(Calendar.YEAR, 1990);
+        fecha1.set(Calendar.MONTH, Calendar.JANUARY);
+        fecha1.set(Calendar.DATE, 4);
+        customer.setBirthday(fecha1.getTime());
+        customer.setGender(Gender.MALE);
+        customer.setCard(Card.MASTERCARD);
+        customer.seteMail("abc@def.com");
+        customer.setId(1);
+
+        Customer customer2 = new Customer();
+        customer2.setName("TestCustomer");
+        Calendar fecha2 = Calendar.getInstance();
+        fecha2.setTimeInMillis(0);
+        fecha2.set(Calendar.YEAR, 1991);
+        fecha2.set(Calendar.MONTH, Calendar.MAY);
+        fecha2.set(Calendar.DATE, 5);
+        customer2.setBirthday(fecha2.getTime());
+        customer2.setGender(Gender.MALE);
+        customer2.setCard(Card.MASTERCARD);
+        customer2.seteMail("ghi@jkl.es");
+        customer2.setId(2);
+
+        customerServiceImpl.save(customer);
+        customerServiceImpl.save(customer2);
+
+        listaClientes = new ArrayList();
+        listaClientes.add(customer);
+        listaClientes.add(customer2);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of count method, of class CustomerServiceImpl.
+     */
+    public void testCount() {
+        long expResult = listaClientes.size();
+        long result = customerServiceImpl.count();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of findAll method, of class CustomerServiceImpl.
+     */
+    public void testFindAll() {
+        List expResult = listaClientes;
+        List result = customerServiceImpl.findAll();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of findById method, of class CustomerServiceImpl.
+     */
+    public void testFindById() {
+        
+        Customer expResult = null;
+        Customer result = customerServiceImpl.findById(null);
+        assertEquals(expResult, result);
+        
+        expResult = null;
+        result = customerServiceImpl.findById(-10);
+        assertEquals(expResult, result);
+        
+        expResult = (Customer)listaClientes.get(0);
+        result = customerServiceImpl.findById(1);
+        assertEquals(expResult, result);
+    }
+}
