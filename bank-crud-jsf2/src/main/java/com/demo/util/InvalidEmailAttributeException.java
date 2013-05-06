14bd1a678bef8b8a479fbc3d6342d9561cc34824
jdrodp <jdrodp@gmail.com>
2013-05-06 06:59:13 +0000

    Agregadas excepciones personalizadas Autores: Jeisson Andrés Vergara Vargas, Johan David Rodríguez Portela Solicitante: Edward Alexander Rojas Clavijo
    Agregado excepciones personalizadas
    Package: util

---
+++ bank-crud-jsf2/src/main/java/com/demo/util/InvalidEmailAttributeException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.util;
+
+/**
+ *
+ * @author Johan
+ */
+public class InvalidEmailAttributeException extends Exception {
+    
+    public InvalidEmailAttributeException(String msg) {
+        super(msg);
+    } 
+}
---
+++ bank-crud-jsf2/src/main/java/com/demo/util/InvalidNameAttributeException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.util;
+
+/**
+ *
+ * @author Johan
+ */
+public class InvalidNameAttributeException extends Exception {
+
+    public InvalidNameAttributeException(String msg) {
+        super(msg);
+    }
+}
+
