c3d87613757fd7d6be158cc76461cb7a628e2de4
sacortesh <sacortesh@97d5706b-13d2-8499-cc0b-5c95a49f2aa6>
2013-05-07 23:37:20 +0000

    Cambio en el codigo para tener en cuenta nuevos mensajes de excepciones mas especificos. Clase: CustomerServiceImpl
    Autor: Sergio Andrés Cortés Hernández
    Solicitante: Edward Alexander Rojas Clavijo
    
    Agregada sobre verificacion sobre campos que podrian ser ingresados como vacios y causar excepciones en el despliegue de la aplicacion.
    
    Autor: Sergio Andrés Cortés Hernández
    Solicitante: Edward Alexander Rojas Clavijo

--- bank-crud-jsf2/src/main/java/com/demo/model/service/CustomerServiceImpl.java
+++ bank-crud-jsf2/src/main/java/com/demo/model/service/CustomerServiceImpl.java
@@ -28,7 +28,6 @@ public class CustomerServiceImpl implements ICustomerService {
     public final static int MAX_LENGTH_NAME = 30;
     public final static int MAX_LENGTH_ABOUT = 250;
     public final static int ADULT_AGE = 18;
-    
     @ManagedProperty("#{customerDAO}")
     private ICrudDAO<Customer, Integer> dao;
 
@@ -77,11 +76,7 @@ public class CustomerServiceImpl implements ICustomerService {
 
     @Override
     public void save(Customer entity) throws RequiredAttributeException {
-        try {
-            this.dao.save(entity);
-        } catch (Exception ex) {
-            throw new RequiredAttributeException("Trying to persist an object without one or many empty (null) attributes");
-        }
+        this.dao.save(entity);
     }
 
     public void setDao(ICrudDAO<Customer, Integer> dao) {
@@ -91,26 +86,35 @@ public class CustomerServiceImpl implements ICustomerService {
     @Override
     public Customer update(Customer entity) throws Exception {
         Customer ret = this.dao.findById(entity.getId());
-        if (isChangingGender(entity)) {
+        if (entity.getGender() == null) {
+            throw new RequiredAttributeException("Trying to put a null gender");
+        } else if (isChangingGender(entity)) {
             throw new GenreNotEditableException("Trying to change the gender of a customer");
+        } else if (entity.getName() == null) {
+            throw new RequiredAttributeException("Trying to put a null name");
         } else if (entity.getName().length() <= MIN_LENGTH_NAME) {
             throw new InvalidNameAttributeException("Trying to put a length Name less than 5!");
         } else if (entity.getName().length() >= MAX_LENGTH_NAME) {
             throw new InvalidNameAttributeException("Trying to put a length Name higher than 30!");
         } else if (entity.getBirthday() == null) {
             throw new InvalidBirthdayAttributeException("Trying to put a null Birthday!");
-        } else if (entity.getAbout().length() > 250) {
+        } else if (entity.getAbout() != null && entity.getAbout().length() > 250) {
             throw new InvalidAboutAttributeException("Trying to put a length About higher than 250!");
         } else if (entity.getCard() == null) {
+            //En modificación es requerida
             throw new RequiredAttributeException("Trying to put a null Card!");
         } else if (entity.getMailingList() == null) {
+            //TODO discutir si por defecto hacer la mailing list false en vez de nula cuando esto ocurra
             throw new RequiredAttributeException("Trying to put a null Mailing List!");
         } else if (entity.getBirthday() == null) {
             throw new RequiredAttributeException("Trying to put a null Birthday!");
         } else if (isNotAdult(entity)) {
             throw new InvalidBirthdayAttributeException("Trying to put a underage customer!");
+        } else if (entity.geteMail() == null) {
+            throw new RequiredAttributeException("Trying to put a null Mail!");
         } else if (isNotValidEmail(entity)) {
             throw new InvalidBirthdayAttributeException("Trying to put a invalid eMail!");
+
         } else {
             ret = this.dao.update(entity);
         }
@@ -129,26 +133,26 @@ public class CustomerServiceImpl implements ICustomerService {
         return findById(customer.getId()).getGender() != customer.getGender()
                 ? true : false;
     }
-    
+
     private boolean isNotAdult(Customer customer) {
         int factor = 0;
         Calendar cal = Calendar.getInstance();
         Calendar today = Calendar.getInstance();
         cal.setTime(customer.getBirthday());
-        
+
         if (today.get(Calendar.DAY_OF_YEAR) < cal.get(Calendar.DAY_OF_YEAR)) {
             factor = -1;
         }
-        
+
         int age = today.get(Calendar.YEAR) - cal.get(Calendar.YEAR) + factor;
-        
+
         if (age >= ADULT_AGE) {
             return false;
         }
-        return true;       
+        return true;
     }
-    
-    private boolean isNotValidEmail(Customer customer) {      
+
+    private boolean isNotValidEmail(Customer customer) {
         String re = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*"
                 + "|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\"
                 + "\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*"
