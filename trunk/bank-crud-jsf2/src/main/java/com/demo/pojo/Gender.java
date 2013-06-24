cdefb50fdc834f5b12182c9af27b08f9abdd0c72
sacortesh <sacortesh@97d5706b-13d2-8499-cc0b-5c95a49f2aa6>
2013-06-24 16:48:04 +0000

    Cambio en el formato del codigo para evitar las llaves iniciales en las lineas Paquetes: com.demo.pojo
    Autor: Sergio Andrés Cortés Hernández
    Solicitante: Edward Alexander Rojas Clavijo
    
    Agregada sobre verificacion sobre campos que podrian ser ingresados como vacios y causar excepciones en el despliegue de la aplicacion.
    
    Autor: Sergio Andrés Cortés Hernández
    Solicitante: Edward Alexander Rojas Clavijo

--- trunk/bank-crud-jsf2/src/main/java/com/demo/pojo/Gender.java
+++ trunk/bank-crud-jsf2/src/main/java/com/demo/pojo/Gender.java
@@ -3,29 +3,23 @@
  */
 package com.demo.pojo;
 
-public enum Gender
-{
+public enum Gender {
+
     MALE(0, "MALE"),
     FEMALE(1, "FEMALE");
 
-    public static Gender fromId(int id) throws IllegalArgumentException
-    {
-        for (Gender e : values())
-        {
-            if (e.id == id)
-            {
+    public static Gender fromId(int id) throws IllegalArgumentException {
+        for (Gender e : values()) {
+            if (e.id == id) {
                 return e;
             }
         }
         throw new IllegalArgumentException(String.format("Gender with id %s doesn't exists", id));
     }
 
-    public static Gender fromLabel(String label) throws IllegalArgumentException
-    {
-        for (Gender e : values())
-        {
-            if (e.label.equalsIgnoreCase(label))
-            {
+    public static Gender fromLabel(String label) throws IllegalArgumentException {
+        for (Gender e : values()) {
+            if (e.label.equalsIgnoreCase(label)) {
                 return e;
             }
         }
@@ -34,25 +28,21 @@ public enum Gender
     private final int id;
     private final String label;
 
-    private Gender(int id, String label)
-    {
+    private Gender(int id, String label) {
         this.id = id;
         this.label = label;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return this.id;
     }
 
-    public String getLabel()
-    {
+    public String getLabel() {
         return this.label;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return this.id + ":" + this.label;
     }
 }
